26182efe4b9437f57d2f737d25f4c9bd51a2e1bb
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2024-08-14 01:32:43 -0700

    Update Transformer.py

---
+++ Transformer.py
@@ -0,0 +1,56 @@
+import torch
+from torch import nn
+import math
+
+#1. Input Embeddings layer
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+#2. Positional Encoding layer
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        #create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        #create a vector of shape
+        position = torch.arrange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arrange(0,d_model),2).float() * (math.log(10000.0 / d_model))
+        
+        #apply the sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsquuze(0) #(1, seq_len, d_model)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self,x):
+        x = x+ (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+#3. Add/Norm Layer Normalization layer
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-.6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) #Added
+
+    def forward(self, x):
+        main = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim =-1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.ops) + self.bias
